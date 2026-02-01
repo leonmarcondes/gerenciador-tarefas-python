08b8b1173ec3126714f320c1cc3d1abb204c1374
Leonardo Marcondes <leonardo_marcondes28@outlook.com>
2026-02-01 18:51:50 -0300

    Cria funções de carregamento e salvamento de tarefas

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-# Gerenciador de Tarefas em Python
+import json
+
+ARQUIVO_TAREFAS = "tarefas.json"
+
+
+def carregar_tarefas():
+    try:
+        with open(ARQUIVO_TAREFAS, "r", encoding="utf-8") as arquivo:
+            return json.load(arquivo)
+    except FileNotFoundError:
+        return []
+
+
+def salvar_tarefas(tarefas):
+    with open(ARQUIVO_TAREFAS, "w", encoding="utf-8") as arquivo:
+        json.dump(tarefas, arquivo, ensure_ascii=False, indent=4)
