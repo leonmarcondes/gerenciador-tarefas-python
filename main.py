dbbe1c992723696fd092809d446e727405c8a03b
Leonardo Marcondes <leonardo_marcondes28@outlook.com>
2026-02-01 18:52:51 -0300

    Adiciona menu principal do sistema

--- main.py
+++ main.py
@@ -14,3 +14,12 @@ def carregar_tarefas():
 def salvar_tarefas(tarefas):
     with open(ARQUIVO_TAREFAS, "w", encoding="utf-8") as arquivo:
         json.dump(tarefas, arquivo, ensure_ascii=False, indent=4)
+
+
+def mostrar_menu():
+    print("\n=== GERENCIADOR DE TAREFAS ===")
+    print("1 - Adicionar tarefa")
+    print("2 - Listar tarefas")
+    print("3 - Marcar tarefa como concluída")
+    print("4 - Remover tarefa")
+    print("0 - Sair")
